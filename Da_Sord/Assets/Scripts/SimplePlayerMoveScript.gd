bfc36349d52821dac1816bd38167dc5983aef29a
Woody Wyche <woodywyche@greenestpastures.com.au>
2022-11-21 19:14:10 +0800

    Parallax & Platformer Movement
    Has simple parallax backgrounds and platformer-style movement

---
+++ Da_Sord/Assets/Scripts/SimplePlayerMoveScript.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+export (float) var speed = 1
+var velocity = Vector2.ZERO
+var jumpPow = 600
+const GRAVITY = 30
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left"):
+		velocity.x = -speed
+		$Sprite.flip_h = true
+	elif Input.is_action_pressed("right"):
+		velocity.x = speed
+		$Sprite.flip_h = false
+
+	velocity.y = velocity.y + GRAVITY
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = -jumpPow
+	velocity = move_and_slide(velocity,Vector2.UP)
+	velocity.x = lerp(velocity.x, 0, 0.2)
+
