d345e1af2c276be3d871324119116fc9caac98b6
Woody Wyche <woodywyche@greenestpastures.com.au>
2022-11-29 15:34:27 +0800

    Added Basic DialogueBox & Font
    Created a simple dialogue box with temporary assets including
    -An msPaint Speech Bubble
    -Bookxel Font https://howyoudoing.itch.io/bookxel 
    
    Need to implement text scrolling/ 'next' on click/button/etc.

--- Da_Sord/Assets/Scripts/SimplePlayerMoveScript.gd
+++ Da_Sord/Assets/Scripts/SimplePlayerMoveScript.gd
@@ -4,6 +4,7 @@ export (float) var speed = 1
 var velocity = Vector2.ZERO
 export (float) var jumpPow = 600
 const GRAVITY = 30
+var bump = [0, 0]
 
 func _physics_process(delta):
 	if Input.is_action_pressed("left"):
@@ -14,10 +15,23 @@ func _physics_process(delta):
 		$Sprite.flip_h = false
 
 	velocity.y = velocity.y + GRAVITY
-	if Input.is_action_just_pressed("jump") && is_on_floor():
+	if Input.is_action_just_pressed("jump") && is_on_floor() && !Input.is_action_pressed("down"):
 		velocity.y = -jumpPow
 		$AnimationPlayer.play("KickFlip")
+	elif Input.is_action_pressed("down") && Input.is_action_just_pressed("jump"):
+		Drop()
 	velocity = move_and_slide(velocity,Vector2.UP)
+	
 	velocity.x = lerp(velocity.x, 0, 0.2)
+
+func Drop():
+	for i in get_slide_count():
+		bump = get_slide_collision(i).collider
+	for i in bump.get_groups().size():
+		if bump.get_groups()[i] == "Platform":
+			bump.get_node("CollisionShape2D").disabled = true
+			yield(get_tree().create_timer(.3), "timeout")
+			bump.get_node("CollisionShape2D").disabled = false
+			
 
 
