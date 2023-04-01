574b230175b3d46c41935e83b7547a56abfa03bd
Woody Wyche <woodywyche@greenestpastures.com.au>
2023-04-01 10:59:12 +0800

    Fixed Procedural Generation
    Tiles now have capacity to join up post generation

--- Da_Sord/Assets/Scripts/DungeonGenerator.gd
+++ Da_Sord/Assets/Scripts/DungeonGenerator.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
-const Up = 0x1
-const Right = 0x2
-const Down = 0x4
-const Left = 0x8
+export var startPos = Vector2(0,0)
 
-export var cell_walls = {Vector2(0, -1): Up, Vector2(1, 0): Right,
-				  Vector2(0, 1): Down, Vector2(-1, 0): Left}
+const UP = 0x1
+const RIGHT = 0x2
+const DOWN = 0x4
+const LEFT = 0x8
+
+export var cell_walls = {Vector2(0, -1): UP, Vector2(1, 0): RIGHT,
+				  Vector2(0, 1): DOWN, Vector2(-1, 0): LEFT}
 
 var tile_size = 64  # tile size (in pixels)
 export var width = 25  # width of map (in tiles)
@@ -26,8 +28,7 @@ func check_neighbors(cell, unvisited):
 	# returns an array of cell's unvisited neighbors
 	var list = []
 	for n in cell_walls.keys():
-		if cell + n in unvisited && Map.get_cellv(cell) - cell_walls[n] > 0 && Map.get_cellv(cell) - cell_walls[n] < 16:
-#			print(Map.get_cellv(cell), cell_walls[n])
+		if cell + n in unvisited:
 			list.append(cell + n)
 	return list
 	
@@ -40,43 +41,38 @@ func check_neighbors(cell, unvisited):
 func make_maze():
 	var unvisited = []  # array of unvisited tiles
 	var stack = []
-	var wallHavers = []
 	# fill the map with solid tiles
 	Map.clear()
 	for x in range(width):
 		for y in range(height):
 			unvisited.append(Vector2(x, y))
-			Map.set_cellv(Vector2(x, y), Up|Right|Down|Left)
-	var current = Vector2(0, 0)
+			Map.set_cellv(Vector2(x, y), UP|RIGHT|DOWN|LEFT)
+	var current = Vector2(startPos.x-1, startPos.y-1)
+#	var current = Vector2(randi() % int(startPos.x-1), randi() % int(startPos.y-1))
 	unvisited.erase(current)
 	while unvisited:
 		var neighbors = check_neighbors(current, unvisited)
-		var walls = check_neighbors(current, wallHavers)
+		var walls = check_neighbors(current, stack)
 		if neighbors.size() > 0:
 			var next = neighbors[randi() % neighbors.size()]
-			#erase previous before check // Remove extra from WallHavers
 			if walls.size() > 2:
-				var newHole = walls[randi() % walls.size()]
-				var dir2 = newHole - current
-				var current_walls2 = Map.get_cellv(current) - cell_walls[dir2]
-				var next_walls2 = Map.get_cellv(newHole) - cell_walls[-dir2]
-				print(current, ", ", Map.get_cellv(current), ", ", dir2)
-#				print(current, ", ", current_walls2, ", ", dir2)
-#				print(newHole, ", ", next_walls2, ", ", dir2)
-				Map.set_cellv(current, current_walls2)
-				Map.set_cellv(newHole, next_walls2)
-				
+				var neighborino = cell_walls.keys()[randi() % cell_walls.size()]
+				if Map.get_cellv(current) & cell_walls[neighborino]:
+					var thisWalls = Map.get_cellv(current) - cell_walls[neighborino]
+					var thatWalls = Map.get_cellv(current+neighborino) - cell_walls[-neighborino]
+					Map.set_cellv(current, thisWalls)
+					Map.set_cellv(current+neighborino, thatWalls)
 			stack.append(current)
 			# remove walls from *both* cells
 			var dir = next - current
 			var current_walls = Map.get_cellv(current) - cell_walls[dir]
 			var next_walls = Map.get_cellv(next) - cell_walls[-dir]
-			Map.set_cellv(current, current_walls)
-			Map.set_cellv(next, next_walls)
+			if Map.get_cellv(current) & cell_walls[dir]:
+				Map.set_cellv(current, current_walls)
+				Map.set_cellv(next, next_walls)
 			current = next
 			unvisited.erase(current)
-			wallHavers.append(stack.back())
-			print(cell_walls[dir])
+#			print(cell_walls[dir])
 			
 		elif stack:
 			current = stack.pop_back()
