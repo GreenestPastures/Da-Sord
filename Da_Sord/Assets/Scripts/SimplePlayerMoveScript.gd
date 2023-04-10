812323320d36a63018070fa176655d4f9315922c
Woody Wyche <woodywyche@greenestpastures.com.au>
2023-04-10 22:01:24 +0800

    Collisions & Double Jump
    -All collisions for the basic leveld implemented
    -Added Double Jump

--- Da_Sord/Assets/Scripts/SimplePlayerMoveScript.gd
+++ Da_Sord/Assets/Scripts/SimplePlayerMoveScript.gd
@@ -6,6 +6,7 @@ var velocity = Vector2.ZERO
 export (float) var jumpPow = 600
 const GRAVITY = 30
 var bump = [0, 0]
+var jumpCount
 
 
 #func _input(event):
@@ -20,12 +21,19 @@ func _physics_process(delta):
 		velocity.x = speed
 		$Sprite.flip_h = false
 	
+	if is_on_floor():
+		jumpCount = 1
+	
 	velocity.y = velocity.y + GRAVITY
 	if Input.is_action_just_pressed("jump") && is_on_floor() && !Input.is_action_pressed("down"):
 		velocity.y = -jumpPow
-		$AnimationPlayer.play("KickFlip")
+#		$AnimationPlayer.play("KickFlip")
 	elif Input.is_action_pressed("down") && Input.is_action_just_pressed("jump"):
 		Drop()
+	elif Input.is_action_just_pressed("jump") && jumpCount:
+		velocity.y = -jumpPow
+		jumpCount-= 1
+		$AnimationPlayer.play("KickFlip")
 	velocity = move_and_slide(velocity,Vector2.UP)
 	
 	velocity.x = lerp(velocity.x, 0, 0.2)
