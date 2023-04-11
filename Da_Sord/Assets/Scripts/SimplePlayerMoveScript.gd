4f0825178c0ef7028d2e6ced07e56447ca5553b4
Woody Wyche <woodywyche@greenestpastures.com.au>
2023-04-11 13:31:45 +0800

    Added dashing and Walljumps
    Added simple dash and is_on_wall() walljumps

--- Da_Sord/Assets/Scripts/SimplePlayerMoveScript.gd
+++ Da_Sord/Assets/Scripts/SimplePlayerMoveScript.gd
@@ -4,9 +4,17 @@ export (float) var speed = 1.0
 var stance = 1
 var velocity = Vector2.ZERO
 export (float) var jumpPow = 600
+export (float) var dashPow = 10000
 const GRAVITY = 30
 var bump = [0, 0]
 var jumpCount
+var canJump = true
+var crouch = false
+var midDash = false
+var canDash = true
+export var dashCooldown = .6
+onready var curshape = $StandShape
+onready var halfspeed = speed*0.5
 
 
 #func _input(event):
@@ -14,26 +22,54 @@ var jumpCount
 
 
 func _physics_process(delta):
-	if Input.is_action_pressed("left"):
-		velocity.x = -speed
-		$Sprite.flip_h = true
-	elif Input.is_action_pressed("right"):
-		velocity.x = speed
-		$Sprite.flip_h = false
+	if !midDash:
+		if Input.is_action_pressed("left"):
+			velocity.x = -speed
+			$Sprite.flip_h = true
+			$CrouchSprite.flip_h = true
+		elif Input.is_action_pressed("right"):
+			velocity.x = speed
+			$Sprite.flip_h = false
+			$CrouchSprite.flip_h = false
 	
-	if is_on_floor():
+	if is_on_floor() || is_on_wall():
 		jumpCount = 1
 	
+	if Input.is_action_just_pressed("dash") && canDash:
+		if $Sprite.flip_h == true:
+			Dashing()
+			velocity.x = -dashPow
+		else:
+			Dashing()
+			velocity.x = dashPow
+	
+	
+	
+	
+	if Input.is_action_pressed("down"):
+		$AnimationPlayer.play("Crouch")
+		curshape = $CrouchShape
+		crouch = true
+	elif crouch == true:
+		$AnimationPlayer.play("Stand")
+		curshape = $StandShape
+		crouch = false
+	
+	
 	velocity.y = velocity.y + GRAVITY
-	if Input.is_action_just_pressed("jump") && is_on_floor() && !Input.is_action_pressed("down"):
+	if is_on_wall() && !Input.is_action_pressed("down"):
+		velocity.y = clamp(velocity.y, 60, -10)
+	if Input.is_action_just_pressed("jump") && is_on_floor() && !Input.is_action_pressed("down") && canJump:
 		velocity.y = -jumpPow
+		JC()
 #		$AnimationPlayer.play("KickFlip")
 	elif Input.is_action_pressed("down") && Input.is_action_just_pressed("jump"):
 		Drop()
-	elif Input.is_action_just_pressed("jump") && jumpCount:
+	elif Input.is_action_just_pressed("jump") && jumpCount && canJump:
 		velocity.y = -jumpPow
 		jumpCount-= 1
 		$AnimationPlayer.play("KickFlip")
+		JC()
 	velocity = move_and_slide(velocity,Vector2.UP)
 	
 	velocity.x = lerp(velocity.x, 0, 0.2)
@@ -43,7 +79,6 @@ func _physics_process(delta):
 		stance = 1
 		$AnimationPlayer.play("StanceHeavy")
 	if Input.is_action_just_pressed("Stance 2"):
-		print("Stanced Down")
 		stance = 2
 		$AnimationPlayer.play("StanceLight")
 		
@@ -54,12 +89,10 @@ func _physics_process(delta):
 			$AnimationPlayer.play("Swing 2H")
 	if Input.is_action_just_pressed("light attack"):
 		if stance == 1:
-			print("smack")
 			$AnimationPlayer.play("Swing 1L")
 			
 		elif stance == 2:
 			$AnimationPlayer.play("Swing 2L")
-			print("kerthwack!!")
 
 func Drop():
 	if get_slide_count():
@@ -68,11 +101,21 @@ func Drop():
 		if bump.get_groups().size():
 			for i in bump.get_groups().size():
 				if bump.get_groups()[i] == "Platform":
-					bump.get_node("CollisionShape2D").get_parent().set_collision_layer_bit(2, false)
+					bump.get_node("curShape").get_parent().set_collision_layer_bit(2, false)
 					yield(get_tree().create_timer(.3), "timeout")
-					bump.get_node("CollisionShape2D").get_parent().set_collision_layer_bit(2, true)
+					bump.get_node("curShape").get_parent().set_collision_layer_bit(2, true)
 
-#func PA():
+func Dashing():
+	midDash = true
+	canDash = false
+	yield(get_tree().create_timer(.1), "timeout")
+	midDash = false
+	yield(get_tree().create_timer(dashCooldown), "timeout")
+	canDash = true
 	
 
-
+func JC():
+	canJump = false
+	yield(get_tree().create_timer(.3), "timeout")
+	canJump = true
+	
