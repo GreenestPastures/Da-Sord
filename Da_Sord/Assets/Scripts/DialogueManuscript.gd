124109d15af311c49e906c517c7d9aa9d1b83524
Woody Wyche <woodywyche@greenestpastures.com.au>
2023-04-21 14:27:28 +0800

    Fixed Opening Dialogue
    Dialog only opens/starts if it hasn't already

--- Da_Sord/Assets/Scripts/DialogueManuscript.gd
+++ Da_Sord/Assets/Scripts/DialogueManuscript.gd
@@ -4,6 +4,8 @@ export (Array, Array, String) var choice = []
 export (int) onready var lines = sentences.size()
 export (int) onready var lastLine = lines-1
 export (bool) var questQuestions 
+export (bool) var playerTalking = false
+var tempPlayer
 var currentCol
 
 
@@ -18,12 +20,14 @@ func _on_QuestGiver_body_entered(touching):
 	if touching.is_in_group("Player"):
 		questQuestions = true
 		currentCol = touching
+		tempPlayer = touching
 
 
 func _on_QuestGiver_body_exited(area):
 	if area.is_in_group("Player"):
 		questQuestions = false
+		playerTalking = false
 
 func _input(event):
-	if Input.is_action_just_pressed("interact") && questQuestions:
+	if Input.is_action_just_pressed("interact") && questQuestions && !tempPlayer.get_node("Camera2D/CanvasLayer/DialogueItem").visible:
 		currentCol.get_node("Camera2D/CanvasLayer/DialogueItem").questDialog(sentences, lines, choice)
